c37b60e510e8abe6b4acbca9d989ad3751efe86f
Douglas Mendes <douglasaleixomendes@gmail.com>
2021-05-21 20:32:51 -0300

    initial commit

---
+++ api-community/src/main.ts
@@ -0,0 +1,12 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.SERVER_PORT || 3040);
+  new Logger(bootstrap.name).log(
+    `Api gateway running in port ${process.env.SERVER_PORT || 3040}`,
+  );
+}
+bootstrap();
